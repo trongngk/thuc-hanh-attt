9bcb9a23aeb52541cf2fa8deedcb7110fd423e9f
trongngk <53163510+trongngk@users.noreply.github.com>
2021-06-09 07:51:57 +0700

    Update 9. sàng nguyên tố.py

--- 9. sàng nguyên tố.py
+++ 9. sàng nguyên tố.py
@@ -1,14 +1,11 @@
 def snt(n):
-  check=[True]*(n+1)
+  check = [True]*(n+1)
+  s = []
   for i in range(2,n+1):
     if check[i]:
       for j in range(i*i,n+1,i):
-        check[j]=False
-  s=[]
-  for i in range(2,n+1):
-    if check[i]:
+        check[j] = False
       s.append(i)
   return s
 n=int(input('Nhap n = '))
 print(snt(n))
-
